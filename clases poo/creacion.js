7c8b14ea11d9d37326f7d5fe9a50b82a4a5a080d
rosver96 <rosverdelgado@hotmail.com>
2023-06-21 23:21:25 -0500

    punto

--- clases poo/creacion.js
+++ clases poo/creacion.js
@@ -1,12 +1,30 @@
-class Rectangulo {
-    constructor (ancho,altura){
-    this.ancho = ancho;
-    this.altura = altura;
-    }
+// class Rectangulo {
+//     constructor (ancho,altura){
+//     this.ancho = ancho;
+//     this.altura = altura;
+//     }
 
-    calcularArea(){
-        return this.ancho * this.altura;
+//     calcularArea(){
+//         return this.ancho * this.altura;
+//     }
+// }
+// const rectangulo1 = new Rectangulo(5,3);
+// console.log(rectangulo1.calcularArea());
+
+
+
+
+
+class Triangulo {
+    constructor (base,lado1){
+        this.base = base;
+        this.lado1 = lado1;
+      
+    }
+    calcular(){
+        return this.base** this.lado1;
     }
 }
-const rectangulo1 = new Rectangulo(5,3);
-console.log(rectangulo1.calcularArea());
+
+const resultado = new Triangulo(2,3);
+console.log(resultado.calcular());
