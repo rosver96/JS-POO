4dff43d6509e52166038b9eb4d0ac47c4209108a
rosver96 <rosverdelgado@hotmail.com>
2023-06-23 00:06:35 -0500

    cambio1

---
+++ objetos-class_2/QUIZ - SOLUCION/quiz.js
@@ -0,0 +1,13 @@
+class Pregunta {
+
+    constructor(pregunta,opcionA,opcionB,opcionC,opcionD) {
+        this.Pregunta = pregunta;
+        this.opcionA = opcionA;
+        this.opcionB = opcionB;
+        this.opcionB = opcionC;
+        this.opcionB = opcionD;
+    }   
+
+    
+
+}
