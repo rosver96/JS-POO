65987f8443c5e1f3449a05115d801495b06ca553
rosver96 <rosverdelgado@hotmail.com>
2023-06-24 01:03:46 -0500

    cambio3

--- objetos-class_2/QUIZ - SOLUCION/quiz.js
+++ objetos-class_2/QUIZ - SOLUCION/quiz.js
@@ -1,39 +1,116 @@
 class Pregunta {
-    constructor(pregunta,opcionA,opcionB,opcionC,opcionD) {
-        this.Pregunta = pregunta;
-        this.opcionA = opcionA;
-        this.opcionB = opcionB;
-        this.opcionB = opcionC;
-        this.opcionB = opcionD;
-
-    }   
-
-}
-
-class Manager {
-    constructor() {
-        this.lista = [];
+    constructor(pregunta,opciona,opcionb,opcionc,opciond,correct){
+        this.pregta = pregunta;
+        this.opcA = opciona;
+        this.opcB = opcionb;
+        this.opcC = opcionc;
+        this.opcD = opciond;
+        this.correcta = correct;
     }
 }
 
-const pregunta = document.getElementById("pregunta").value;
-const opciona = document.getElementById("opciona").value; 
-const opcionb = document.getElementById("opcionb").value;  
-const opcionc = document.getElementById("opcionc").value; 
-const opciond = document.getElementById("opciond").value;   
-const lista = document.getElementById("lista").value;
 
-formulario.addEventlistener("submit", (e) => {
-    e.preventDefault();
-    
-    const pregunta1 = new Pregunta;
-    const opciona1 =  opciona;
-    const opcionb2 =  opcionb;
-    const opcionc3 =  opcionc;
-    const opciond4 =  opciond;
-})
+class GestorPreguntas{
+    constructor(){
+        this.lista = [];
+    }
+    //metodo agregar
+    agregarpregunta(pregunta){
+        this.lista.push(pregunta);
+    }
 
-function question() {
-    lista.innerHTML = "";
-    const lista = manager
+    //metodo eliminar
+    eliminarpregunta(index){
+        this.lista.splice(index, 1);
+        listar();
+    }
+}
+
+// crear la instancia para cada pregunta
+const preguntanueva = new GestorPreguntas();
+
+// llamo los inputs del formulario
+const formulario = document.getElementById('formulario');
+const preguntainput = document.getElementById('preguntaH');
+const opcionAinput = document.getElementById('opcionA');
+const opcionBinput = document.getElementById('opcionB');
+const opcionCinput = document.getElementById('opcionC');
+const opcionDinput = document.getElementById('opcionD');
+const correctainput = document.getElementById('opccorrecta')
+
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const pregta = preguntainput.value;
+    const opcA = opcionAinput.value;
+    const opcB = opcionBinput.value;
+    const opcC = opcionCinput.value;
+    const opcD = opcionDinput.value;
+    const correcta = correctainput.value;
+
+    const nuevapegunta = new Pregunta(pregta,opcA,opcB,opcC,opcD);
+    preguntanueva.agregarpregunta(nuevapegunta);
+    listar();
+
+    formulario.reset();
+});
+
+
+const padrepreguntas = document.getElementById('padrepreguntas');
+
+function listar(){
+    padrepreguntas.innerHTML = "";
+
+    preguntanueva.lista.forEach((pregunta, index) => {
+        const hijopreguntas = document.createElement('div');
+        hijopreguntas.className = 'card';
+        hijopreguntas.style.width = '20rem';
+
+        hijopreguntas.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${pregunta.pregta}
+        </h5>
+        <div class="card-text">
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+            ${pregunta.opcA}
+            </label>
+        </div>
+        <div class="form-check">
+            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">
+            ${pregunta.opcB}
+            </label>
+        </div>
+        <div class="form-check">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+        <label class="form-check-label" for="flexRadioDefault2">
+            ${pregunta.opcC}
+        </label>
+        </div>
+        <div class="form-check">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+        <label class="form-check-label" for="flexRadioDefault2">
+        ${pregunta.opcD}
+        </label>
+        </div>
+        </div>
+        <button onclick="eliminarpreguntas"(${index})">
+        eliminar
+        </button>
+        </div>
+        </div>`
+
+        padrepreguntas.appendChild(hijopreguntas);
+        
+    });
+}
+
+
+function eliminarpreguntas(index){
+    preguntanueva.eliminarpregunta(index)
+    listar();
 }
