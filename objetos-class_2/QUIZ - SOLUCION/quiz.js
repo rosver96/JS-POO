36e795b413fb28b705b4e1b09d025ce36cd1c24c
rosver96 <rosverdelgado@hotmail.com>
2023-06-24 21:16:50 -0500

    codigo completo

--- objetos-class_2/QUIZ - SOLUCION/quiz.js
+++ objetos-class_2/QUIZ - SOLUCION/quiz.js
@@ -1,116 +1,164 @@
 class Pregunta {
-    constructor(pregunta,opciona,opcionb,opcionc,opciond,correct){
-        this.pregta = pregunta;
-        this.opcA = opciona;
-        this.opcB = opcionb;
-        this.opcC = opcionc;
-        this.opcD = opciond;
-        this.correcta = correct;
+    constructor(pregunta, opciona, opcionb, opcionc, opciond, correcta) {
+      this.pregunta = pregunta;
+      this.opcA = opciona;
+      this.opcB = opcionb;
+      this.opcC = opcionc;
+      this.opcD = opciond;
+      this.correcta = correcta;
     }
-}
-
-
-class GestorPreguntas{
-    constructor(){
-        this.lista = [];
+  }
+  
+  class GestorPreguntas {
+    constructor() {
+      this.lista = [];
     }
-    //metodo agregar
-    agregarpregunta(pregunta){
-        this.lista.push(pregunta);
+  
+    agregarPregunta(pregunta) {
+      this.lista.push(pregunta);
     }
-
-    //metodo eliminar
-    eliminarpregunta(index){
-        this.lista.splice(index, 1);
-        listar();
+  
+    eliminarPregunta(index) {
+      this.lista.splice(index, 1);
     }
-}
-
-// crear la instancia para cada pregunta
-const preguntanueva = new GestorPreguntas();
-
-// llamo los inputs del formulario
-const formulario = document.getElementById('formulario');
-const preguntainput = document.getElementById('preguntaH');
-const opcionAinput = document.getElementById('opcionA');
-const opcionBinput = document.getElementById('opcionB');
-const opcionCinput = document.getElementById('opcionC');
-const opcionDinput = document.getElementById('opcionD');
-const correctainput = document.getElementById('opccorrecta')
-
-
-formulario.addEventListener('submit', (event) => {
+  
+    actualizarPregunta(index, pregunta, opciona, opcionb, opcionc, opciond, correcta) {
+      this.lista[index].pregunta = pregunta;
+      this.lista[index].opcA = opciona;
+      this.lista[index].opcB = opcionb;
+      this.lista[index].opcC = opcionc;
+      this.lista[index].opcD = opciond;
+      this.lista[index].correcta = correcta;
+    }
+  }
+  
+  const gestorPreguntas = new GestorPreguntas();
+  const formulario = document.getElementById('formulario');
+  const preguntaInput = document.getElementById('preguntaH');
+  const opcionAInput = document.getElementById('opcionA');
+  const opcionBInput = document.getElementById('opcionB');
+  const opcionCInput = document.getElementById('opcionC');
+  const opcionDInput = document.getElementById('opcionD');
+  const correctaInput = document.getElementById('opccorrecta');
+  const padrepreguntas = document.getElementById('padrepreguntas');
+  
+  let indiceEditando = -1;
+  
+  formulario.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    const pregta = preguntainput.value;
-    const opcA = opcionAinput.value;
-    const opcB = opcionBinput.value;
-    const opcC = opcionCinput.value;
-    const opcD = opcionDinput.value;
-    const correcta = correctainput.value;
-
-    const nuevapegunta = new Pregunta(pregta,opcA,opcB,opcC,opcD);
-    preguntanueva.agregarpregunta(nuevapegunta);
+  
+    const pregunta = preguntaInput.value;
+    const opcA = opcionAInput.value;
+    const opcB = opcionBInput.value;
+    const opcC = opcionCInput.value;
+    const opcD = opcionDInput.value;
+    const correcta = correctaInput.value;
+  
+    const nuevaPregunta = new Pregunta(pregunta, opcA, opcB, opcC, opcD, correcta);
+  
+    if (indiceEditando === -1) {
+      gestorPreguntas.agregarPregunta(nuevaPregunta);
+    } else {
+      gestorPreguntas.actualizarPregunta(
+        indiceEditando,
+        nuevaPregunta.pregunta,
+        nuevaPregunta.opcA,
+        nuevaPregunta.opcB,
+        nuevaPregunta.opcC,
+        nuevaPregunta.opcD,
+        nuevaPregunta.correcta
+      );
+      indiceEditando = -1;
+    }
+  
     listar();
-
+  
     formulario.reset();
-});
-
-
-const padrepreguntas = document.getElementById('padrepreguntas');
-
-function listar(){
+  });
+  
+  function listar() {
     padrepreguntas.innerHTML = "";
-
-    preguntanueva.lista.forEach((pregunta, index) => {
-        const hijopreguntas = document.createElement('div');
-        hijopreguntas.className = 'card';
-        hijopreguntas.style.width = '20rem';
-
-        hijopreguntas.innerHTML = `
-        <div class="card" style="width: 18rem;">
-        <div class="card-body">
-        <h5 class="card-title">${pregunta.pregta}
-        </h5>
-        <div class="card-text">
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-            <label class="form-check-label" for="flexRadioDefault1">
-            ${pregunta.opcA}
-            </label>
-        </div>
-        <div class="form-check">
-            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-            <label class="form-check-label" for="flexRadioDefault2">
-            ${pregunta.opcB}
-            </label>
-        </div>
-        <div class="form-check">
-        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-        <label class="form-check-label" for="flexRadioDefault2">
-            ${pregunta.opcC}
-        </label>
-        </div>
-        <div class="form-check">
-        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-        <label class="form-check-label" for="flexRadioDefault2">
-        ${pregunta.opcD}
-        </label>
-        </div>
-        </div>
-        <button onclick="eliminarpreguntas"(${index})">
-        eliminar
-        </button>
-        </div>
-        </div>`
-
-        padrepreguntas.appendChild(hijopreguntas);
-        
+  
+    gestorPreguntas.lista.forEach((pregunta, index) => {
+      const hijopreguntas = document.createElement('div');
+      hijopreguntas.className = 'card';
+      hijopreguntas.style.width = '20rem';
+  
+      hijopreguntas.innerHTML = `
+        <div class="card text-white bg-dark mb-3" style="max-width: 20rem;">
+          <div class="card-body">
+            <h5 class="card-title">${pregunta.pregunta}</h5>
+            <div class="card-text" style="color:white">
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                <label class="form-check-label" for="flexRadioDefault1">${pregunta.opcA}</label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                <label class="form-check-label" for="flexRadioDefault2">${pregunta.opcB}</label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3">
+                <label class="form-check-label" for="flexRadioDefault3">${pregunta.opcC}</label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault4">
+                <label class="form-check-label" for="flexRadioDefault4">${pregunta.opcD}</label>
+              </div>
+            </div>
+            <button onclick="eliminarPregunta(${index})" class="btn btn-danger">Eliminar</button>
+            <button onclick="editarPregunta(${index})" class="btn btn-success">Editar</button>
+          </div>
+        </div>`;
+  
+      padrepreguntas.appendChild(hijopreguntas);
     });
-}
-
-
-function eliminarpreguntas(index){
-    preguntanueva.eliminarpregunta(index)
+  }
+  
+  function eliminarPregunta(index) {
+    gestorPreguntas.eliminarPregunta(index);
     listar();
-}
+  }
+  
+  function editarPregunta(index) {
+    const pregunta = gestorPreguntas.lista[index];
+    preguntaInput.value = pregunta.pregunta;
+    opcionAInput.value = pregunta.opcA;
+    opcionBInput.value = pregunta.opcB;
+    opcionCInput.value = pregunta.opcC;
+    opcionDInput.value = pregunta.opcD;
+    correctaInput.value = pregunta.correcta;
+  
+    indiceEditando = index;
+  
+    formulario.onsubmit = (event) => {
+      event.preventDefault();
+  
+      const nuevaPregunta = new Pregunta(
+        preguntaInput.value,
+        opcionAInput.value,
+        opcionBInput.value,
+        opcionCInput.value,
+        opcionDInput.value,
+        correctaInput.value
+      );
+  
+      gestorPreguntas.actualizarPregunta(
+        indiceEditando,
+        nuevaPregunta.pregunta,
+        nuevaPregunta.opcA,
+        nuevaPregunta.opcB,
+        nuevaPregunta.opcC,
+        nuevaPregunta.opcD,
+        nuevaPregunta.correcta
+      );
+      listar();
+  
+      formulario.reset();
+      formulario.onsubmit = null;
+      indiceEditando = -1;
+    };
+  }
+  
+  listar();
+  
