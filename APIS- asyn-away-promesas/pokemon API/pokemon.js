fe52c894e258ccbd7775249c19eccba6b026aac9
WingDark <losadabolivar@gmail.com>
2023-07-07 20:33:52 -0500

    pokemon

--- APIS- asyn-away-promesas/pokemon API/pokemon.js
+++ APIS- asyn-away-promesas/pokemon API/pokemon.js
@@ -1,32 +1,77 @@
- 
-async function pokem(){
-try {
 
-const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+let url = 'https://pokeapi.co/api/v2/pokemon/'
 
-const data = await response .json();
-return data; 
-
-} catch (error) {
-    throw 'eror al obtener datos'
+const pokemon = async(url,i) => {
+    const response = await fetch(url + i);   // fetch con esto entro a la api//
+    const data = await response.json();       // accedo al json y traigo la info en formato json
+    functionMostarPokemon(data)
 }
 
-}
-
-async function pokem2(){
-    try {
-        const data = await pokem();
-        const main = document.getElementById('main');
-        console.log(data)
-        const datopokemon =`
-        <main class="main">
-
-        </main>
-        `
-        main .innerHTML = datopokemon
-    }catch(error) {
-        console.log(error);
+const llamarPokemon = async() => {
+    for (let i = 1; i<100; i++){
+        await pokemon(url, i); 
     }
 }
+llamarPokemon();
 
-pokem2();
+
+
+const contenedorHtml = document.querySelector('#contenedor');
+
+function functionMostarPokemon(pokemones){
+    const crearDiv = document.createElement('div');
+    crearDiv.classList.add('clasePokemon');
+    crearDiv.innerHTML = ` 
+    <div class="card" style="width: 16rem;">
+        <img src="${pokemones.sprites.other ["official-artwork"].front_default}" class="card-img-top" alt="">
+        <p> Tipo: ${pokemones.types["0"].type.name}</p>
+        <p> Name : ${pokemones.name}</p>
+        <p> Altura: ${pokemones.height}</p>
+        <p> Peso: ${pokemones.weight}</p>
+    `
+    contenedorHtml.append(crearDiv);
+}
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+// async function pokem(){
+// try {
+
+// const response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+
+// const data = await response .json();
+// return data; 
+
+// } catch (error) {
+//     throw 'eror al obtener datos'
+// }
+
+// }
+
+// async function pokem2(){
+//     try {
+//         const data = await pokem();
+//         const main = document.getElementById('main');
+//         console.log(data)
+//         const datopokemon =`
+//         <main class="main">
+
+//         </main>
+//         `
+//         main .innerHTML = datopokemon
+//     }catch(error) {
+//         console.log(error);
+//     }
+// }
+
+// pokem2();
